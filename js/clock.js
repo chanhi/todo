2800694158f3c2c25db1f15b183f4fc79459ed0c
chanhi <61691088+chanhi@users.noreply.github.com>
2021-12-07 14:21:55 +0000

    modify

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("#js-clock h2");
 function getTime() {
     const date = new Date();
     const minutes = String(date.getMinutes()).padStart(2,"0");
-    const hours = String(date.getHours().)padStart(2,"0");
+    const hours = String(date.getHours()).padStart(2,"0");
     //const seconds = String(date.getSeconds().)padStart(2,"0");
     //new Date().getSeconds().toString().padStart(2,"0");  기능 같음
     clock.innerText = `${hours}:${minutes}`;
